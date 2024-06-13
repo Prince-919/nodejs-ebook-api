385dc6f16dd2f97a25298351f8cd20d1c62a32b6
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-13 23:21:47 +0530

    add password hash

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
 import userModal from "./userModal";
+import bcrypt from "bcrypt";
 
 const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
@@ -15,6 +16,8 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
     const error = createHttpError(400, "User already exists with this email.");
     return next(error);
   }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
 
   res.json({
     message: "User created",
