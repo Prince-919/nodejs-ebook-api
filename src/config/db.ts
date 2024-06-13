d40e07d1f2fff05645e599a0a50aebb051b41261
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-13 19:16:00 +0530

    add database setup

---
+++ src/config/db.ts
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+import { config } from "./config";
+
+const connectDB = async () => {
+  try {
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to database successfully.");
+    });
+
+    mongoose.connection.on("error", (error) => {
+      console.log("Error to connecting to database", error);
+    });
+    await mongoose.connect(config.databaseUrl as string);
+  } catch (error) {
+    console.error("Failed to connect to database.", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
