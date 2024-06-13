a3c70909cf1cf4f8fcaacb13c03672eb4624e6de
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-13 21:47:49 +0530

    add global error handler

---
+++ src/middlewares/globalErrorHandler.ts
@@ -0,0 +1,19 @@
+import { HttpError } from "http-errors";
+import { NextFunction, Request, Response } from "express";
+import { config } from "../config/config";
+
+const globalErrorHandler = (
+  err: HttpError,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const statusCode = err.statusCode || 500;
+
+  return res.status(statusCode).json({
+    message: err.message,
+    stack: config.env === "development" ? err.stack : "",
+  });
+};
+
+export default globalErrorHandler;
