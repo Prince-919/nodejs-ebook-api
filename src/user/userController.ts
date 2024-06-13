ea61cd5c4ec9ac843fbf9fcb17ad9e8eb74c06ee
Prince Sharma <prince.sharma_cs20@gla.ac.in>
2024-06-13 23:14:57 +0530

    add user modal

--- src/user/userController.ts
+++ src/user/userController.ts
@@ -1,11 +1,18 @@
 import { NextFunction, Request, Response } from "express";
 import createHttpError from "http-errors";
+import userModal from "./userModal";
 
-const createUser = (req: Request, res: Response, next: NextFunction) => {
+const createUser = async (req: Request, res: Response, next: NextFunction) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
     const error = createHttpError(400, "All fields are required");
+    return next(error);
+  }
+
+  const user = await userModal.findOne({ email });
+  if (user) {
+    const error = createHttpError(400, "User already exists with this email.");
     return next(error);
   }
 
